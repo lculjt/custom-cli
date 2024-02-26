6de3dfab98e1f884370e590246f10fba3101aa9f
lijianting <jianting.li@qiyuesuo.com>
2024-02-26 20:56:50 +0800

    feat: 发布项目

--- bin/cli.js
+++ bin/cli.js
@@ -1,3 +1,44 @@
 #! /usr/bin/env node
+const createApp = require('../lib/create.js');
 
-console.log('custom-cli working ~')
+const { Command } = require('commander');
+
+const program = new Command();
+
+program
+  .command('create <app-name>')
+  .description('create a new project')
+  .option('-f, --force', 'overwrite target directory if it exist') // 是否强制创建，当文件夹已经存在
+  .action((name, options) => {
+    // 在 create.js 中执行创建任务
+    createApp(name, options)
+  })
+
+
+// 配置 config 命令
+program
+  .command('config [value]')
+  .description('inspect and modify the config')
+  .option('-g, --get <path>', 'get value from option')
+  .option('-s, --set <path> <value>')
+  .option('-d, --delete <path>', 'delete option from config')
+  .action((value, options) => {
+    console.log(value, options)
+  })
+
+// 配置 ui 命令
+program
+  .command('ui')
+  .description('start add open roc-cli ui')
+  .option('-p, --port <port>', 'Port used for the UI Server')
+  .action((option) => {
+    console.log(option)
+  })
+
+program
+  // 配置版本号信息
+  .version(`v${require('../package.json').version}`)
+  .usage('<command> [option]')
+  
+// 解析用户执行命令传入参数
+program.parse(process.argv);
